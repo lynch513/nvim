29352f17cf8e86fd234d474d2d0d29229d4e3fbd
lynch513 <lynch513@yandex.ru>
2025-02-05 22:41:26 +0500

    skip some diagnostics in lua

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -188,6 +188,9 @@ return {
             },
             -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
             -- diagnostics = { disable = { 'missing-fields' } },
+            diagnostics = {
+              globals = { 'vim' },
+            },
           },
         },
       },
